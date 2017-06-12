3f1684df238ae18c9dbbbf24bb1240c247c920f0
Vincent Tellier <vincent@tellier.me>
2017-06-12 19:50:38 +0000

    Log trace is print to confirm success

--- src/ordhuset-app/mi-utils.js
+++ src/ordhuset-app/mi-utils.js
@@ -43,6 +43,31 @@ OhUtils = function(superClass) {
       }
       return ret;
     }
+
+    // Returns true if all arguments are defined and not empty
+    isNotEmpty() {
+      var ret = true;
+      for (var i = 0; ret && i < arguments.length; i++) {
+        var variable = arguments[i];
+        if (typeof variable !== 'undefined' && variable != null) {
+          if (Array.isArray(variable)) {
+            ret = variable.length > 0;
+          }
+          else if(!isNaN(variable)) {
+            ret = variable > 0;
+          }
+          else if(typeof variable === 'string' || variable instanceof String) {
+            ret = variable.length > 0;
+          }
+          else {
+            ret = Object.keys(variable).length !== 0;
+          }
+        }
+        else
+          ret = false;
+      }
+      return ret;
+    }
     
     stringify(obj) {
       return JSON.stringify(obj, 2);
